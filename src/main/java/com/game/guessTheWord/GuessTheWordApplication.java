2a0b8cdf31b24ac398d9ae8841aa14d6ddd3e4dc
Samim Hossain <samimhossain825@gmail.com>
2024-04-10 04:20:06 +0600

    Guess The word Game initialized

---
+++ src/main/java/com/game/guessTheWord/GuessTheWordApplication.java
@@ -0,0 +1,13 @@
+package com.game.guessTheWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessTheWordApplication.class, args);
+	}
+
+}
