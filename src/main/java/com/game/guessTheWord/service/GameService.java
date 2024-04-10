bc038688c550fabcca032355c8856237ab471e3d
Samim Hossain <samimhossain825@gmail.com>
2024-04-10 13:03:42 +0600

    populate dashed string for randomlySelected word

--- src/main/java/com/game/guessTheWord/service/GameService.java
+++ src/main/java/com/game/guessTheWord/service/GameService.java
@@ -2,17 +2,33 @@ package com.game.guessTheWord.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Random;
 
 @Service
 public class GameService {
 
+    public String randomlySelectedWord;
     public String[] randomWords = {"father", "mother", "sister", "hello", "world", "light", "java","html"};
-    Random random = new Random();
+    private Random random = new Random();
+    private char[] allCharactersOfTheWord;
+
+    public GameService(){
+        randomlySelectedWord = randomWords[random.nextInt(randomWords.length)];
+        System.out.println("Randomly selected word is : "+ randomlySelectedWord);
+        allCharactersOfTheWord = new char[randomlySelectedWord.length()];
+    }
 
     @Override
     public String toString() {
-        return randomWords[random.nextInt(randomWords.length)];
+
+        String ret = "";
+        for (char c: allCharactersOfTheWord) {
+            if(c == '\u0000') {
+                ret = ret + "_";
+            }
+            ret = ret + " ";
+        }
+
+        return ret;
     }
 }
