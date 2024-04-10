d04aef8cf50f6d2f311830e28661f89cb2ac49fe
Samim Hossain <samimhossain825@gmail.com>
2024-04-10 23:08:18 +0600

    guessedCharacter input field added

--- src/main/java/com/game/guessTheWord/controller/GameController.java
+++ src/main/java/com/game/guessTheWord/controller/GameController.java
@@ -4,10 +4,7 @@ import com.game.guessTheWord.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class GameController {
@@ -17,8 +14,9 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping("/game-home")
-    public String showGameHomePage(Model model) {
+    public String showGameHomePage(Model model, @RequestParam(value = "guessedChar", required = false) String guessedChar) {
 
+        System.out.println("The guessed character is : "+guessedChar);
         String randomWord = gameService.toString();
         model.addAttribute("randomWord", randomWord);
         return "game-home-page";
